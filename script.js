2593da5a5a81f0926955d12fead372cdda4c16a9
Sigolletes <juditreverter87@gmail.com>
2022-11-10 10:25:50 +0100

    Do display

---
+++ script.js
@@ -0,0 +1,20 @@
+// FUNCTIONS
+
+    // Module - Gameboard Object that stores the gameboard
+    
+    // Factory - Players object
+
+    // Module - Display Controller - Object to control the flow of the game
+
+
+// USER INTERFACE
+
+    // Render the contents of the gameboard array
+
+    // Interaction with gameboard
+
+    // Check for Game Over
+
+    // Clean the Interface, users names, button to start/ restart the game / congratulation winner
+
+// IA    
