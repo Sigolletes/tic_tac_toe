27909ae899208f04baedf12b5ea140e4a08e59d9
Sigolletes <juditreverter87@gmail.com>
2022-11-11 15:46:28 +0100

    Do more functions

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Global DOM variables
+
+const nameContainer = document.querySelector("#nameContainer");
+const winnerContainer = document.querySelector("#winnerContainer");
+
 // FUNCTIONS
 
     // Module - Gameboard Object that stores the gameboard
@@ -12,7 +17,6 @@ const gameboardModule = (function() {
             gameboard[index] = "O";
         }
     }
-
     return {
         gameboard,
         mark
@@ -20,39 +24,25 @@ const gameboardModule = (function() {
 })();
 let board = gameboardModule;
 
-    
     // Factory - Players object
 
-    // SOLVE ADD POINTS FUNCTION
-
 const playerFactory = (name, human) => {
-
     let points = 0;
-
-/*     const addPoints = () => {
-        points = (this.points + 1);
-        console.log(this.points);
-    } */
 
     const winner = () => {
         const winnerText = document.querySelector("#winnerText");
-        winnerText.innerText = this.name;
+        winnerText.innerText = name;
     }
-
     return { name, human, points, winner };
 }
 let player1 = playerFactory("Player 1", true);
 let player2 = playerFactory("Player2", false);
-
 let userTurn = player1.name;
-
 
     // Module - Display Controller - Object to control the flow of the game
 
 const controller = (function() {
-
     function checkWinner(user) {
-
         if (board.gameboard[0] === user && board.gameboard[1] === user && board.gameboard[2] === user || 
             board.gameboard[3] === user && board.gameboard[4] === user && board.gameboard[5] === user || 
             board.gameboard[6] === user && board.gameboard[7] === user && board.gameboard[8] === user || 
@@ -65,25 +55,24 @@ const controller = (function() {
                 if (user === "X") {
                     player1.points = player1.points + 1;
                     scoreboard.scoreboardUpdate();
+                    renderGameboard.cleanGameboard();
+                    player1.winner();
+                    winnerContainer.style.display = "flex";
                 } else {
-                    player2.addPoints();
+                    player2.points = player2.points + 1;
                     scoreboard.scoreboardUpdate();
+                    renderGameboard.cleanGameboard();
+                    player2.winner();
+                    winnerContainer.style.display = "flex";
                 }
         }
     }
-
     return {
         checkWinner
     }
-
 })();
 
-
 // USER INTERFACE
-
-    // Global DOM variables
-
-const nameContainer = document.querySelector("#nameContainer");
 
         // Board positions
 const p0 = document.querySelector("#p0");
@@ -101,10 +90,8 @@ const p8 = document.querySelector("#p8");
 const opponentChoice = (() => {
     let userChoice = document.querySelector("#userChoice");
     let AIChoice = document.querySelector("#AIChoice");
-
     const userAdversary = document.querySelector("#userAdversary");
     const AIAdversary = document.querySelector("#AIAdversary");
-
     const chooseContainer = document.querySelector("#chooseContainer");
 
     userChoice.addEventListener("click", () => {
@@ -113,7 +100,6 @@ const opponentChoice = (() => {
         chooseContainer.style.display = "none";
         nameContainer.style.display = "flex";
     });
-
     AIChoice.addEventListener("click", () => {
         AIAdversary.style.display = "flex";
         userAdversary.style.display = "none";
@@ -128,12 +114,10 @@ const nameChoice = (() => {
     const name1 = document.querySelector("#name1");
     const name2 = document.querySelector("#name2");
     const startGame = document.querySelector("#startGame");
-
     const titlePlayer1 = document.querySelector("#titlePlayer1");
     const titlePlayer2 = document.querySelector("#titlePlayer2");
 
     startGame.addEventListener("click", () => {
-
         if (name1.value !== "") {
             player1.name = name1.value;
             titlePlayer1.innerText = player1.name;
@@ -142,8 +126,8 @@ const nameChoice = (() => {
             player2.name = name2.value;
             titlePlayer2.innerText = player2.name;
         } 
-
         nameContainer.style.display = "none";
+        gameboardInteraction.boardEvent();
     });
 })();
 
@@ -172,8 +156,16 @@ const renderGameboard = (() => {
         board.gameboard[6] = "";
         board.gameboard[7] = "";
         board.gameboard[8] = "";
+        p0.style.cursor = "pointer";
+        p1.style.cursor = "pointer";
+        p2.style.cursor = "pointer";
+        p3.style.cursor = "pointer";
+        p4.style.cursor = "pointer";
+        p5.style.cursor = "pointer";
+        p6.style.cursor = "pointer";
+        p7.style.cursor = "pointer";
+        p8.style.cursor = "pointer";
     }
-
     return {
         render,
         cleanGameboard
@@ -185,25 +177,20 @@ const renderGameboard = (() => {
 const gameboardInteraction = (() => {
     const turn1 = document.querySelector("#turn1");
     const turn2 = document.querySelector("#turn2");
-
     const side1 = document.querySelector("#side1");
     const side2 = document.querySelector("#side2");
 
     function changeTurn() {
         if (userTurn === player1.name) {
             userTurn = player2.name;
-
             turn1.style.display = "none";
             turn2.style.display = "flex";
-
             side1.classList.remove("turn");
             side2.classList.add("turn");
         } else {
             userTurn = player1.name;
-
             turn2.style.display = "none";
             turn1.style.display = "flex";
-
             side2.classList.remove("turn");
             side1.classList.add("turn");
         }
@@ -218,35 +205,39 @@ const gameboardInteraction = (() => {
         controller.checkWinner("O");
     }
 
-    p0.addEventListener("click", () => {
-        movement(0, p0);  
-    }, { once: true });
-    p1.addEventListener("click", () => {
-        movement(1, p1);
-    }, { once: true });
-    p2.addEventListener("click", () => {
-        movement(2, p2);
-    }, { once: true });
-    p3.addEventListener("click", () => {
-        movement(3, p3);
-    }, { once: true });
-    p4.addEventListener("click", () => {
-        movement(4, p4);
-    }, { once: true });
-    p5.addEventListener("click", () => {
-        movement(5, p5);
-    }, { once: true });
-    p6.addEventListener("click", () => {
-        movement(6, p6);
-    }, { once: true });
-    p7.addEventListener("click", () => {
-        movement(7, p7);
-    }, { once: true });
-    p8.addEventListener("click", () => {
-        movement(8, p8);
-    });
+    function boardEvent() {
+        p0.addEventListener("click", () => {
+            movement(0, p0);  
+        }, { once: true });
+        p1.addEventListener("click", () => {
+            movement(1, p1);
+        }, { once: true });
+        p2.addEventListener("click", () => {
+            movement(2, p2);
+        }, { once: true });
+        p3.addEventListener("click", () => {
+            movement(3, p3);
+        }, { once: true });
+        p4.addEventListener("click", () => {
+            movement(4, p4);
+        }, { once: true });
+        p5.addEventListener("click", () => {
+            movement(5, p5);
+        }, { once: true });
+        p6.addEventListener("click", () => {
+            movement(6, p6);
+        }, { once: true });
+        p7.addEventListener("click", () => {
+            movement(7, p7);
+        }, { once: true });
+        p8.addEventListener("click", () => {
+            movement(8, p8);
+        });
+    }
+    return {
+        boardEvent
+    }
 })();
-
 
     // Scoreboard display
 
@@ -258,10 +249,21 @@ const scoreboard = (() => {
         points1.innerText = player1.points;
         points2.innerText = player2.points;
     }
-
     return {
         scoreboardUpdate
     }
+})();
+
+    // Return button from winner container
+
+const returnButton = (() => {
+    const returnBttn = document.querySelector("#return");
+
+    returnBttn.addEventListener("click", () => {
+        winnerContainer.style.display = "none";
+        renderGameboard.render();
+        gameboardInteraction.boardEvent();
+    })
 })();
 
     // Check for Game Over
