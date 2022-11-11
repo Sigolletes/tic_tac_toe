949055f7e4061f3679afc5598c471c17020ecbfb
Sigolletes <juditreverter87@gmail.com>
2022-11-11 10:53:03 +0100

    Add logical and UI functions and improve some style

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+// GLOBAL LOGICAL VARIABLES
+
+let player1 = "Player 1";
+let player2 = "Player 2";
+
+let userTurn = player1;
+
 // FUNCTIONS
 
     // Module - Gameboard Object that stores the gameboard
 
 const gameboardModule = (function() {
-    let gameboard = ["m","","X","","","","O","",""];
+    let gameboard = ["","","X","","","","O","",""];
 
-    const mark = (index, user) => {
-        if (user == player1) {
+    const mark = (index) => {
+        if (userTurn === player1) {
             gameboard[index] = "X";
         } else {
             gameboard[index] = "O";
@@ -18,8 +25,7 @@ const gameboardModule = (function() {
         mark
     }
 })();
-board = gameboardModule;
-console.log(board);
+let board = gameboardModule;
 
     
     // Factory - Players object
@@ -35,11 +41,9 @@ const playerFactory = (name, human) => {
         winnerText.innerText = this.name;
     }
 
-    return { name, human, points, winner};
+    return { name, human, points, addPoints, winner };
 }
 
-let player1 = "Player 1";
-let player2 = "Player 2";
 
     // Module - Display Controller - Object to control the flow of the game
 
@@ -49,6 +53,17 @@ let player2 = "Player 2";
     // Global DOM variables
 
 const nameContainer = document.querySelector("#nameContainer");
+
+        // Board positions
+const p0 = document.querySelector("#p0");
+const p1 = document.querySelector("#p1");
+const p2 = document.querySelector("#p2");
+const p3 = document.querySelector("#p3");
+const p4 = document.querySelector("#p4");
+const p5 = document.querySelector("#p5");
+const p6 = document.querySelector("#p6");
+const p7 = document.querySelector("#p7");
+const p8 = document.querySelector("#p8");
 
     // Adversary choice
 
@@ -102,29 +117,72 @@ const nameChoice = (() => {
     // Render the contents of the gameboard array
 
 const renderGameboard = (() => {
-    const p0 = document.querySelector("#p0");
-    const p1 = document.querySelector("#p1");
-    const p2 = document.querySelector("#p2");
-    const p3 = document.querySelector("#p3");
-    const p4 = document.querySelector("#p4");
-    const p5 = document.querySelector("#p5");
-    const p6 = document.querySelector("#p6");
-    const p7 = document.querySelector("#p7");
-    const p8 = document.querySelector("#p8");
+    function render() {
+        p0.innerText = board.gameboard[0];
+        p1.innerText = board.gameboard[1];
+        p2.innerText = board.gameboard[2];
+        p3.innerText = board.gameboard[3];
+        p4.innerText = board.gameboard[4];
+        p5.innerText = board.gameboard[5];
+        p6.innerText = board.gameboard[6];
+        p7.innerText = board.gameboard[7];
+        p8.innerText = board.gameboard[8];
+    }
 
-    p0.innerText = board.gameboard[0];
-    p1.innerText = board.gameboard[1];
-    p2.innerText = board.gameboard[2];
-    p3.innerText = board.gameboard[3];
-    p4.innerText = board.gameboard[4];
-    p5.innerText = board.gameboard[5];
-    p6.innerText = board.gameboard[6];
-    p7.innerText = board.gameboard[7];
-    p8.innerText = board.gameboard[8];
+    function cleanGameboard() {
+        board.gameboard[0] = "";
+        p1.innerText = "";
+        p2.innerText = "";
+        p3.innerText = "";
+        p4.innerText = "";
+        p5.innerText = "";
+        p6.innerText = "";
+        p7.innerText = "";
+        p8.innerText = "";
+    }
+
+    return {
+        render,
+        cleanGameboard
+    }
 })();
-
+renderGameboard.cleanGameboard();
 
     // Interaction with gameboard
+
+const gameboardInteraction = (() => {
+    function changeTurn() {
+        if (userTurn === player1) {
+            userTurn = player2;
+        } else {
+            userTurn = player1;
+        }
+    }
+
+    function movement(index, position) {
+   
+        board.mark(index);
+        renderGameboard.render();
+        position.onclick = null;
+        changeTurn();
+/* SOLVE THE PROPAGATION .............................................................*/
+        e.preventDefault();
+        console.log(userTurn);
+        
+    }
+
+    p0.addEventListener("click", movement(0, p0), false);
+    
+    p0.onclick = movement(0, p0);
+    p1.onclick = movement(1, p1);
+    p2.onclick = movement(2, p2);
+    p3.onclick = movement(3, p3);
+    p4.onclick = movement(4, p4);
+    p5.onclick = movement(5, p5);
+    p6.onclick = movement(6, p6);
+    p7.onclick = movement(7, p7);
+    p8.onclick = movement(8, p8);
+})();
 
     // Check for Game Over
 
